0b8559f10f9ea52d78f68e94470d8eca4905afd4
Rai <raijindaladitya@gmail.com>
2026-01-18 11:51:48 +0530

    made changes

--- L10/xToPowerN.cpp
+++ L10/xToPowerN.cpp
@@ -4,9 +4,26 @@
 using namespace std;
 
 int powCal(int x , int n){
+    //These all are corner cases
+    if(n == 0) return 1;
+    if(x == 0) return 0;
+    if(n == 1) return x;
+    if(x == 1) return 1;
+    if(x == -1 && n%2 == 0) return 1;
+    if(x == -1 && n%2 != 0) return -1;
+
     long binForm = n;
+
+    //If n is -ve
+    if(binForm < 0){
+        x = 1/x;
+        binForm = -binForm;
+
+    }
+
     int ans = 1;
 
+    //Main code
     while(binForm > 0){
         if(binForm % 2 == 1){
             ans *= x;
