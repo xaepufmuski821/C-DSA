4d35d7edb7081871785c0fc191e5ade9a816a906
Rai <raijindaladitya@gmail.com>
2026-01-18 19:16:02 +0530

    made changes

---
+++ LEETCODE/xToPowerN.cpp
@@ -0,0 +1,56 @@
+//Implement pow(x, n), which calculates x raised to the power n (i.e., x^n).
+
+#include <iostream>
+using namespace std;
+
+double powCal(double x , int n){
+    //These all are corner cases
+    if(n == 0) return 1;
+    if(x == 0) return 0;
+    if(n == 1) return x;
+    if(x == 1) return 1;
+    if(x == -1 && n%2 == 0) return 1;
+    if(x == -1 && n%2 != 0) return -1;
+
+    long binForm = n;
+
+    //If n is -ve
+    if(binForm < 0){
+        x = 1/x;
+        binForm = -binForm;
+
+    }
+
+    double ans = 1;
+
+    //Main code
+    while(binForm > 0){
+        if(binForm % 2 == 1){
+            ans *= x;
+
+        }
+
+        x = x*x;
+        binForm /= 2;
+
+    }
+
+    return ans;
+
+}
+
+int main(){
+
+    int x , n;
+
+    cout << "Enter x: ";
+    cin >> x;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << powCal(x , n);
+
+    return 0;
+
+}
