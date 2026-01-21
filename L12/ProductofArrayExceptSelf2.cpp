2a984f9ca8e53287588aa47379f239e807752d3c
Rai <raijindaladitya@gmail.com>
2026-01-21 20:34:43 +0530

    made changes

---
+++ L12/ProductofArrayExceptSelf2.cpp
@@ -0,0 +1,34 @@
+/*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]*/
+
+//Optimised approach
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector <int> vec = {1,2,3,4};
+    int prefix = vec[0] , suffix = vec[vec.size() - 1];
+
+    for(int i = 1 ; i < vec.size() ; i++){
+        prefix = prefix * vec[i - 1];
+
+    }
+
+    for(int j = vec.size() - 2 ; j >= 0 ; j--){
+        suffix = suffix * vec[j + 1];
+
+    }
+
+    return 0;
+
+}
