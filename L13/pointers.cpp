0af3718b6fe5dd1aaec4d1167dda108d5846aea7
Rai <raijindaladitya@gmail.com>
2026-01-23 16:30:39 +0530

    made changes

---
+++ L13/pointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int *ptr = &a;
+
+    cout << &a << endl;
+    cout << ptr << endl;
+
+    // pointer to pointer
+    int ** parentPtr = &ptr;
+    cout << &ptr << endl;
+    cout << parentPtr << endl;
+
+    //derefernce operator
+    cout << *ptr << endl;
+    cout << **parentPtr << endl;
+
+    
+
+    return 0;
+
+}
