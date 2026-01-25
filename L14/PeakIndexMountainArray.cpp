97eb83777bda9e561f064f5c8e6af1743d66fba3
Rai <raijindaladitya@gmail.com>
2026-01-25 18:14:31 +0530

    made changes

---
+++ L14/PeakIndexMountainArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector <int> arr = {1,2,3,4,1,0};
+    int st = 1 , end = arr.size() - 2;
+
+    while(st <= end){
+        int mid = st + (end - st) / 2;
+
+        if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]){
+            cout << "the index of peak: " << mid;
+            break;
+        }   
+
+        if(arr[mid - 1] < arr[mid]){
+            st = mid + 1;
+
+        }else{
+            end = mid - 1;
+
+        }
+
+    }
+
+    return 0;
+
+}
