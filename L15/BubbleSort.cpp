8df09a475538e13b2d3ac67a97a671755bfd37d2
Rai <raijindaladitya@gmail.com>
2026-01-31 20:57:10 +0530

    made changes

---
+++ L15/BubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sorter(vector <int> &vec){
+    int n = vec.size();
+
+    for(int i = 0 ; i < n - 1 ; i++){
+        for(int j = 0 ; j < n-i-1 ; j++){
+            if(vec[j] > vec[j + 1]){
+                swap(vec[j] , vec[j + 1]);
+
+            }
+
+        }
+
+    }
+
+    for(int i : vec){
+        cout << i << " ";
+
+    }
+
+}
+
+int main(){
+
+    vector <int> arr = {4,1,5,2,3};
+
+    sorter(arr);
+
+    return 0;
+
+}
---
+++ L15/SelectionSort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sorter(vector <int> &vec){
+    int n = vec.size();
+
+    for(int i = 0 ; i < n - 1 ; i++){
+        int smallestIndex = i;
+        for(int j = i + 1 ; j < n ; j++){
+            if(vec[smallestIndex] > vec[j]){
+                smallestIndex = j;
+
+            }
+
+        }
+
+        swap(vec[smallestIndex] , vec[i]);
+
+    }
+
+}
+
+int main(){
+
+    vector <int> vec ={4,1,5,2,3};
+
+    sorter(vec);
+
+    for(int i : vec){
+        cout << i << " ";
+
+    }
+
+    return 0;
+
+}
