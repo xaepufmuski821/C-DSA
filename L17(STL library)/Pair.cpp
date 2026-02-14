5bb21e5ec78b2e28086e993aeedcf3c002fad6f7
Rai <raijindaladitya@gmail.com>
2026-02-14 18:55:43 +0530

    made changes

--- L17(STL library)/Pair.cpp
+++ L17(STL library)/Pair.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main(){
 
-    // pair <int , string> p = {1,"hehe"};
-    // cout << p.first << endl;    
-    // cout << p.second << endl;
+    pair <int , string> p = {1,"hehe"};
+    cout << p.first << endl;    
+    cout << p.second << endl;
 
-    // cout << endl;
+    cout << endl;
 
-    // //pair inside pair
-    // pair <int , pair <int , string>> p1 = {3 , {1,"hola amigo"}};
-    // cout << p1.first << endl;
-    // cout << p1.second.first << endl;
-    // cout << p1.second.second << endl;
+    //pair inside pair
+    pair <int , pair <int , string>> p1 = {3 , {1,"hola amigo"}};
+    cout << p1.first << endl;
+    cout << p1.second.first << endl;
+    cout << p1.second.second << endl;
 
     //pair inside vector
 
