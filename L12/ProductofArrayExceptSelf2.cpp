14218d2c0b6f96732ed526658522c1b3323c7d5a
Rai <raijindaladitya@gmail.com>
2026-02-14 14:04:39 +0530

    made changes

--- L12/ProductofArrayExceptSelf2.cpp
+++ L12/ProductofArrayExceptSelf2.cpp
@@ -19,7 +19,7 @@ int main(){
     vector <int> vec = {1,2,3,4};
     int n = vec.size();
     vector <int> ans(n , 1);
-
+    
     for(int i = 1 ; i < vec.size() ; i++){
         ans[i] = ans[i - 1] * vec[i - 1];
 
