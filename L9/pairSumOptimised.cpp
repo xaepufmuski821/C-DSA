4e2dc22a8c24eea60f2893951b118fb0053b622f
Rai <raijindaladitya@gmail.com>
2026-06-22 13:58:28 +0530

    made changes and revising

--- L9/pairSumOptimised.cpp
+++ L9/pairSumOptimised.cpp
@@ -1,44 +1,49 @@
+//Return a pair in sorted array with target sum
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-vector <int> PairSumCalc(vector <int> vec , int target){
+vector <int> PairSum(vector <int> &nums , int target){
+    
+    int st = 0 , end = nums.size()-1;   
 
-    vector <int> ans;
-    int i = 0 , j = vec.size() - 1;
+    while(st < end){
+        int pairSum = nums[st] + nums[end];
 
-    while(i<j){
-        if(vec[i] + vec[j] == target){
-            ans.push_back(vec[i]);
-            ans.push_back(vec[j]);
+        if(pairSum == target){
+            return {nums[st],nums[end]};
 
-            return ans;
-            break;
+        }else if(pairSum < target){
+            st++;
 
-        }
+        }else if(pairSum > target){
+            end--;
 
-        if(vec[i] + vec[j] < target){
-            i++;
-        }
-
-        if(vec[i] + vec[j] > target){
-            j--;
         }
 
     }
 
-    return ans;
+    return {};
 
 }
 
 int main(){
 
-    vector <int> vec = {1,2,3,4,5};
-    int n = 5;
+    vector <int> nums = {1,3,2,4,5};
+
     int target = 9;
 
-    vector <int> ans = PairSumCalc(vec,target);
-    cout << ans[0] << " " << ans[1];
+    sort(nums.begin() , nums.end());
+
+    vector <int> ans = PairSum(nums , target);
+
+    if (!ans.empty()) {
+        cout << ans[0] << " , " << ans[1];
+    } else {
+        cout << "No pair found";
+    }
 
     return 0;
 
