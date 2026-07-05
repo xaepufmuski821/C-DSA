3c571136413ed306472b2444e1445b797bdf236c
Rai <raijindaladitya@gmail.com>
2026-07-05 18:24:32 +0530

    made changes and revising

--- L9/pairSumBrute.cpp
+++ L9/pairSumBrute.cpp
@@ -1,38 +1,42 @@
+//Return a pair in sorted array with target sum
+//Brute Force
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-vector <int> PairCalc(vector <int> nums , int target){
-
-    int n = nums.size();
-    vector <int> ans;
-
-    for(int i = 0; i<n ; i++){
-        for(int j = i+1 ; j<n ; j++){
+vector <int> PairSum(vector <int> &nums , int target){
+    for(int i = 0 ; i < nums.size() ; i++){
+        for(int j = i+1 ; j < nums.size() ; j++){
             if(nums[i] + nums[j] == target){
-                ans.push_back(nums[i]);
-                ans.push_back(nums[j]);
+                return {nums[i] , nums[j]};
 
-                return ans;
             }
 
         }
 
     }
 
-    return ans;
+    return {};
 
 }
 
 int main(){
 
-    vector <int> nums = {1,2,3,4,5};
-    int targetNum = 9;
+    vector <int> nums = {1,3,2,4};
 
-    vector<int> ans = PairCalc(nums , targetNum);
+    int target = 9;
 
-    cout << ans[0] << "," << ans[1];
+    vector <int> ans = PairSum(nums , target);
 
+    if(!ans.empty()){
+        cout << ans[0] << " , " << ans[1];
+
+    }else{
+        cout << "no pair was found";
+
+    }
+    
     return 0;
 
-}
+ }
