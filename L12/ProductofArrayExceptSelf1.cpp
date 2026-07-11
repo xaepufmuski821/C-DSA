57cb3ca04f378dc8b50000678b7b16e28d7572d1
Rai <raijindaladitya@gmail.com>
2026-07-11 10:03:08 +0530

    made changes

--- L12/ProductofArrayExceptSelf1.cpp
+++ L12/ProductofArrayExceptSelf1.cpp
@@ -1,37 +1,38 @@
-/*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
 
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
-You must write an algorithm that runs in O(n) time and without using the division operation.
-
-Example 1:
-Input: nums = [1,2,3,4]
-Output: [24,12,8,6]*/
-
-//Brute approach
+//Brute Approach
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main(){
+vector <int> ProductOfArray(vector <int> &nums){
+    int n = nums.size();
 
-    vector <int> vec = {1,3,4,5};
-    vector <int> ans;
+    vector <int> ans(n , 1);
 
-    for(int i = 0 ; i < vec.size() ; i++){
-        int product = 1;
-        for(int j = 0 ; j < vec.size() ; j++){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
             if(i != j){
-                product *= vec[j];
+                ans[i] *= nums[j];
 
             }
 
         }
 
-        ans.push_back(product);
-
     }
+
+    return ans;
+
+}
+
+int main(){
+
+    vector <int> nums = {1,2,3,4};
+
+    vector <int> ans = ProductOfArray(nums);
 
     for(int i : ans){
         cout << i << " ";
--- L12/ProductofArrayExceptSelf2.cpp
+++ L12/ProductofArrayExceptSelf2.cpp
@@ -33,7 +33,7 @@ int main(){
 
     }
 
-    for(int i = 0 ; i < vec.size() ; i++){
+    for(int i = 0 ; i < ans.size() ; i++){
         cout << ans[i] << " ";
 
     }
