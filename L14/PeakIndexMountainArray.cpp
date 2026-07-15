3816bb9b54c2721f615308ae8018fd3f7b7f3c76
Rai <raijindaladitya@gmail.com>
2026-07-15 20:17:43 +0530

    revising

--- L14/PeakIndexMountainArray.cpp
+++ L14/PeakIndexMountainArray.cpp
@@ -2,28 +2,34 @@
 #include <vector>
 using namespace std;
 
-int main(){
-
-    vector <int> arr = {1,2,3,4,1,0};
-    int st = 1 , end = arr.size() - 2;
+int PeakMountainIndex(vector <int> &nums){
+    int st = 1 , end = nums.size() - 2;
 
     while(st <= end){
-        int mid = st + (end - st) / 2;
+        int mid = st + (end - st)/2;
 
-        if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]){
-            cout << "the index of peak: " << mid;
-            break;
-        }   
-
-        if(arr[mid - 1] < arr[mid]){
+        if(nums[mid-1] < nums[mid] && nums[mid] < nums[mid+1]){
             st = mid + 1;
 
-        }else{
+        }else if(nums[mid-1] > nums[mid]){
             end = mid - 1;
+
+        }else{
+            return mid;
 
         }
 
     }
+
+    return -1;
+
+}
+
+int main(){
+
+    vector <int> nums = {1,2,3,4,3,0};
+
+    cout << PeakMountainIndex(nums);
 
     return 0;
 
